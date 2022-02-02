385924b3b3e891900eac932f36a96e0b261ec67b
Hans299 <batubarahans299@gmail.com>
2022-02-02 15:46:58 +0700

    Learn_react TicTacToo

--- src/index.js
+++ src/index.js
@@ -10,8 +10,5 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-// index javascript untuk mengaktifkan mekanisme dari kerangka kerja app.js
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
